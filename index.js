39faa6a6d12940d018bd6960c47109850e6b3903
jaehyeong park <windowrains@naver.com>
2022-12-08 16:24:03 +0900

    index js 기본설정 완료

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+const root = document.getElementById("root")
+const body = document.body
+const road = document.createElement("div")
+
+road.style.width = "100vw"
+road.style.height = "40vh"
+road.style.backgroundColor = "#333"
+body.appendChild(road)
+
+function createSquare(){
+    let squareSize = 30
+    let squarePostionLeft = body.clientWidth - squareSize
+    let squarePostionTop = root.clientHeight - squareSize
+    let square = document.createElement("div")
+    square.style.width = `${squareSize}px`
+    square.style.height = `${squareSize}px`
+    square.style.backgroundColor = "pink"
+    square.style.position = "absolute"
+    square.style.left = `${squarePostionLeft}px`
+    square.style.top = `${squarePostionTop}px`
+    root.appendChild(square)
+    return square
+}
+
+
